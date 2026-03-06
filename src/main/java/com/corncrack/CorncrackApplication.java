2898d6cb45a33caa36c98877d6046180b2e22c0e
cassianolanger <cassianolangerm@gmail.com>
2026-03-05 22:46:46 -0300

    build

---
+++ src/main/java/com/corncrack/CorncrackApplication.java
@@ -0,0 +1,13 @@
+package com.corncrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorncrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorncrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/corncrack/CorncrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corncrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorncrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
